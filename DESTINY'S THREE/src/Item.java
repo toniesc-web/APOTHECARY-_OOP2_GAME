8f8ebd7dac40b01f41abd4437f5f94f248b6bbd6
ashbatayola <ashleebatayola1129@gmail.com>
2026-05-12 12:30:22 +0800

    feat(items): implement abstract Item class

--- DESTINY'S THREE/src/Item.java
+++ DESTINY'S THREE/src/Item.java
@@ -1,1 +1,14 @@
+public abstract class Item {
+    protected String name;
+    protected String description;
 
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+
+    public abstract void use(Player player);
+}
