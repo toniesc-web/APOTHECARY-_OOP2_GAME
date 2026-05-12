e46346c01474a02855c748424f05bd210318db2a
toniesc-web <antonettheee@gmail.com>
2026-05-12 12:59:34 +0800

    feat: implement Enemy class extending Character

--- DESTINY'S THREE/src/Enemy.java
+++ DESTINY'S THREE/src/Enemy.java
@@ -1,1 +1,11 @@
-
+public class Enemy extends Character {
+    public Enemy(String name, int hp, int attack) {
+        super(name, hp, attack);
+    }
+    
+    @Override
+    public void attack(Character target) {
+        System.out.println(name + " attacks!");
+        target.takeDamage(attack);
+    }
+}
