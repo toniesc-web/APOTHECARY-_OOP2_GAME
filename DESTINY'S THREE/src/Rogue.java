24006da516634692e9d944d30feffef2434112d2
JamesCarlR <jamescarl.rafanan@cit.edu>
2026-05-14 01:48:31 +0800

    Rogue that extends player

--- DESTINY'S THREE/src/Rogue.java
+++ DESTINY'S THREE/src/Rogue.java
@@ -1,1 +1,14 @@
+public class Rogue extends Player {
+    public Rogue(String name) {
+        super(name, 120, 80, 18);
+        skills.add(new Skill("Shadow Strike", 25, 35));
+        skills.add(new Skill("Backstab", 40, 50));
+        skills.add(new Skill("Poison Blade", 15, 25));
+        skills.add(new Skill("Evasion", 5, 0)); // buff placeholder
+    }
 
+    @Override
+    public String getDescription() {
+        return "An agile rogue who strikes swiftly with precision.";
+    }
+}
