fc5b4ed2a03bad3b1256d3fae1f11a7c1e321286
JamesCarlR <jamescarl.rafanan@cit.edu>
2026-05-14 01:50:23 +0800

    Class skill

--- DESTINY'S THREE/src/Skill.java
+++ DESTINY'S THREE/src/Skill.java
@@ -1,1 +1,31 @@
+public class Skill {
+    private String name;
+    private int manaCost;
+    private int damage;
 
+    public Skill(String name, int manaCost, int damage) {
+        this.name = name;
+        this.manaCost = manaCost;
+        this.damage = damage;
+    }
+
+    public String getName() { return name; }
+    public int getManaCost() { return manaCost; }
+    public int getDamage() { return damage; }
+
+    public boolean canUse(Player player) {
+        return player.getMana() >= manaCost;
+    }
+
+    public void use(Player player, Enemy enemy) {
+        if (!canUse(player)) {
+            System.out.println("Not enough mana to use " + name + "!");
+            return;
+        }
+
+        player.setMana(player.getMana() - manaCost);
+        System.out.println(player.getName() + " uses " + name + "!");
+        enemy.takeDamage(damage);
+        System.out.println("Dealt " + damage + " damage!");
+    }
+}
