d5750afe0025291077e1c19f01082bd5e4b9806c
Jsaph227 <brycejosef.cantillas@cit.edu>
2026-05-14 18:51:30 +0800

    feat: completed Weapon.java

--- DESTINY'S THREE/src/Weapon.java
+++ DESTINY'S THREE/src/Weapon.java
@@ -1,1 +1,14 @@
+public class Weapon extends Item {
+    private int attackBonus;
 
+    public Weapon(String name, String description, int attackBonus) {
+        super(name, description);
+        this.attackBonus = attackBonus;
+    }
+
+    @Override
+    public void use(Player player) {
+        player.increaseAttack(attackBonus);
+        System.out.println(player.getName() + " equips " + name + "! Attack +" + attackBonus);
+    }
+}
